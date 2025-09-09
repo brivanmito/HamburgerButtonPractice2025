dd3ce96212982f276b2fe0c2989a2726827ee7c8
Bryan Mino <bryanmt496@gmail.com>
2025-09-09 06:47:11 -0500

    Creating the Nav Bar

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -3,4 +3,9 @@ const navButton = document.querySelector("#ham-btn");
 //Toggle the show class off and on
 navButton.addEventListener('click', () => {
     navButton.classList.toggle('show');
+    //CODE TO THE NAVIGATION BAR
+    navBar.classList.toggle('show');
 })
+
+// CODE FOR THE NAVIGATION BAR
+const navBar = document.querySelector('#nav-bar');
